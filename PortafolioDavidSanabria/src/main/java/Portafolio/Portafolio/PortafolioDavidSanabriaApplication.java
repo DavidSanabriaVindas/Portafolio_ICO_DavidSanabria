87793bd3d23d053f17a064c56886d44d541350d8
DavidSanabriaVindas <dsanabria30061@ufide.ac.cr>
2025-01-22 20:48:26 -0600

    Semana2
    Trabajo en clase semana2

---
+++ PortafolioDavidSanabria/src/main/java/Portafolio/Portafolio/PortafolioDavidSanabriaApplication.java
@@ -0,0 +1,13 @@
+package Portafolio.Portafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioDavidSanabriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioDavidSanabriaApplication.class, args);
+	}
+
+}
